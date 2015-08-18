60e10b63c2538a73dc8ec3ce636b3ed5bf09f524
Dmitry Rodionov <i.am.rodionovd@gmail.com>
2015-08-18 19:43:05 +0700

    Fix setup.py
    Include all subpackages into distrubution.

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 def readme():
 	with open('README.rst') as f:
 		return f.read()
 
 setup(name = 'machobot',
-	version = '0.1.3',
+	version = '0.1.5',
 	description = 'A set of tools for Mach-O executables analysis on OS X',
 	long_description = readme(),
 	url = 'http://github.com/rodionovd/machobot',
 	author = 'Dmitry Rodionov',
 	author_email = 'i.am.rodionovd@gmail.com',
 	license = 'MIT',
-	packages = ['machobot'],
+	packages = find_packages(exclude="tests"),
 	entry_points = {
 		'console_scripts': ['inject_dylib=machobot.command_line:main'],
 	},
